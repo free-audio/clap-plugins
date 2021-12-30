c1a37599b24047622f840d66766e1cc9055debc8
Alexandre BIQUE <bique.alexandre@gmail.com>
2021-12-30 11:36:51 +0100

    Work on the win32 remote channel

--- plugins/io/win32-remote-channel.cc
+++ plugins/io/win32-remote-channel.cc
@@ -1,1 +1,116 @@
+#include <Windows.h>
+
 #include "win32-remote-channel.hh"
+
+namespace clap {
+   struct Win32RemoteChannelOverlapped final {
+      Win32RemoteChannelOverlapped(Win32RemoteChannel &c) : remoteChannel(c) {}
+      OVERLAPPED overlapped{};
+      Win32RemoteChannel &remoteChannel;
+   };
+
+   Win32RemoteChannel::Win32RemoteChannel(const MessageHandler &handler,
+                                          bool cookieHalf,
+                                          void *readHandle,
+                                          void *writeHandle)
+      : super(handler, cookieHalf), _rHandle(readHandle), _wHandle(writeHandle),
+        _rOverlapped(new Win32RemoteChannelOverlapped(*this)),
+        _wOverlapped(new Win32RemoteChannelOverlapped(*this)) {
+      tryReceive();
+   }
+
+   Win32RemoteChannel::~Win32RemoteChannel() { close(); }
+
+   void Win32RemoteChannel::close() {
+      if (_rHandle) {
+         CloseHandle(_rHandle);
+         _rHandle = nullptr;
+      }
+
+      if (_wHandle) {
+         CloseHandle(_wHandle);
+         _wHandle = nullptr;
+      }
+   }
+
+   bool Win32RemoteChannel::isOpen() const noexcept { return _rHandle && _wHandle; }
+
+   void Win32RemoteChannel::tryReceive() {
+      if (_isReceiving)
+         return;
+
+      _isReceiving = true;
+      _inputBuffer.rewind();
+      if (!ReadFileEx(_rHandle,
+                      _inputBuffer.writePtr(),
+                      _inputBuffer.writeAvail(),
+                      &_rOverlapped->overlapped,
+                      &Win32RemoteChannel::receiveCompleted)) {
+         _isReceiving = false;
+         close();
+         return;
+      }
+   }
+
+   void Win32RemoteChannel::receiveCompleted(DWORD dwErrorCode,
+                                             DWORD dwNumberOfBytesTransfered,
+                                             LPOVERLAPPED lpOverlapped) {
+      auto *o = reinterpret_cast<Win32RemoteChannelOverlapped *>(lpOverlapped);
+      auto &c = o->remoteChannel;
+
+      if (dwErrorCode == 0) {
+         assert(c._isReceiving);
+         c._inputBuffer.wrote(dwNumberOfBytesTransfered);
+         c._isReceiving = false;
+         c.processInput();
+         c.tryReceive();
+      } else {
+         c.close();
+      }
+   }
+
+   void Win32RemoteChannel::trySend() {
+      if (_isSending)
+         return;
+
+      while (!_outputBuffers.empty()) {
+         auto &buffer = _outputBuffers.front();
+         const auto avail = buffer.readAvail();
+         if (avail == 0) {
+            _outputBuffers.pop();
+            continue;
+         }
+
+         _isSending = true;
+         if (!WriteFileEx(_wHandle,
+                          buffer.readPtr(),
+                          buffer.readAvail(),
+                          &_wOverlapped->overlapped,
+                          &Win32RemoteChannel::sendCompleted)) {
+            _isSending = false;
+            close();
+         }
+         return;
+      }
+   }
+
+   void Win32RemoteChannel::sendCompleted(DWORD dwErrorCode,
+                                          DWORD dwNumberOfBytesTransfered,
+                                          LPOVERLAPPED lpOverlapped) {
+      auto *o = reinterpret_cast<Win32RemoteChannelOverlapped *>(lpOverlapped);
+      auto &c = o->remoteChannel;
+
+      if (dwErrorCode == 0) {
+         assert(c._isSending);
+         auto &buffer = c._outputBuffers.front();
+         buffer.read(dwNumberOfBytesTransfered);
+         c._isSending = false;
+         c.trySend();
+      } else {
+         c.close();
+      }
+   }
+
+   void Win32RemoteChannel::onError() { close(); }
+
+} // namespace clap
--- plugins/io/win32-remote-channel.hh
+++ plugins/io/win32-remote-channel.hh
@@ -1,9 +1,53 @@
 #pragma once
 
+#include <memory>
+
 #include "basic-remote-channel.hh"
 
+struct _OVERLAPPED;
+
 namespace clap {
-    class Win32RemoteChannel final : public BasicRemoteChannel {
-        public:
-    };
-}
+   struct Win32RemoteChannelOverlapped;
+
+   class Win32RemoteChannel final : public BasicRemoteChannel {
+      using super = BasicRemoteChannel;
+
+   public:
+      Win32RemoteChannel(const MessageHandler &handler,
+                         bool cookieHalf,
+                         void *readHandle,
+                         void *writeHandle);
+      ~Win32RemoteChannel() override;
+
+      void close() override;
+
+      bool isOpen() const noexcept override;
+
+      // Called when there is data to be read, non-blocking
+      void tryReceive() override;
+
+      // Called when data can be written, non-blocking
+      void trySend() override;
+
+      // Called on socket exception
+      void onError() override;
+
+   private:
+      static void receiveCompleted(unsigned long dwErrorCode,
+                                   unsigned long dwNumberOfBytesTransfered,
+                                   _OVERLAPPED *lpOverlapped);
+
+      static void sendCompleted(unsigned long dwErrorCode,
+                                unsigned long dwNumberOfBytesTransfered,
+                                _OVERLAPPED *lpOverlapped);
+
+      void *_rHandle;
+      void *_wHandle;
+
+      bool _isReceiving = false;
+      bool _isSending = false;
+
+      std::unique_ptr<Win32RemoteChannelOverlapped> _rOverlapped;
+      std::unique_ptr<Win32RemoteChannelOverlapped> _wOverlapped;
+   };
+} // namespace clap

5f2db97b9aeebe26b127667d10210e6b4ada144a
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-01-04 00:51:53 +0100

    Should not have commited this

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-rm -rf ~/develop/u-he/plugins/libs/clap
-rm -rf ~/develop/u-he/plugins/libs/clap-helpers
-cp -r ~/develop/clap ~/develop/u-he/plugins/libs/clap
-cp -r ~/develop/clap-helpers ~/develop/u-he/plugins/libs/clap-helpers

7edf772c187e9242201456f3a7bf2af65901c7f6
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-01-15 13:48:17 +0100

    Initial thought on a gui client factory

---
+++ plugins/gui/gui-client-factory.hh
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <memory>
+#include <unordered_map>
+
+#include <QThread>
+#include <QObject>
+
+class QGuiApplication;
+class QThread;
+
+class GuiClientFactory : public QObject
+{
+    using super = QObject;
+
+    Q_OBJECT
+
+public:
+    ~GuiClientFactory();
+    static std::shared_ptr<GuiClientFactory> getInstance() {
+        auto ptr = _instance.lock();
+        if (ptr)
+            return ptr;
+        ptr = std::make_shared<GuiClientFactory>();
+        _instance = ptr;
+        return ptr;
+    }
+
+    uint32_t createGuiClient();
+    void terminteGuiClient(uint32_t clientId);
+
+private:
+    GuiClientFactory();
+
+    void doCreateGuiClient();
+
+    static std::weak_ptr<GuiClientFactory> _instance;
+
+    std::unique_ptr<QThread> _thread;
+    std::unique_ptr<QGuiApplication> _app;
+    std::unordered_map<uint32_t, std::unique_ptr<GuiClient>> _guiClients;
+};

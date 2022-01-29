1288767fa6a3b4b9e8e9cba0c937cf635a56cba3
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-01-29 18:25:30 +0100

    exchange boost archive with msgpack

--- scripts/build.sh
+++ scripts/build.sh
@@ -21,7 +21,7 @@ else
 fi
 
 vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_triplet install --recurse \
-  boost "qtbase[core,png,doubleconversion,gui,concurrent,appstore-compliant${QT_FEATURES}]" \
+  msgpack "qtbase[core,png,doubleconversion,gui,concurrent,appstore-compliant${QT_FEATURES}]" \
   qtdeclarative
 
 cmake --preset $cmake_preset $cmake_triplet

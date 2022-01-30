65061547c1ccb351c0bc9d196d9699be624f942a
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-01-30 22:02:52 +0100

    Better triplet selection

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,24 +4,29 @@ if [[ ! -x vcpkg/vcpkg ]] ; then
   vcpkg/bootstrap-vcpkg.sh
 fi
 
-if [[ "$1" != "" ]]; then
-  vcpkg_triplet="--triplet $1"
-  cmake_triplet="-DVCPKG_TARGET_TRIPLET=$1"
-fi
-
 if [[ $(uname) = Linux ]] ; then
   QT_FEATURES=",xcb,xcb-xlib,xkb,xkbcommon-x11,xlib,xrender,fontconfig,freetype,harfbuzz"
   cmake_preset="ninja-vcpkg"
   gui_model=threaded
+  triplet=$(uname -m)-linux
 elif [[ $(uname) = Darwin ]] ; then
   QT_FEATURES=""
   cmake_preset="ninja-vcpkg"
   gui_model=local
+  triplet=$(uname -m)-osx
 else
   QT_FEATURES=""
   cmake_preset="vs-vcpkg"
   gui_model=local
+  triplet=$(uname -m)-windows
 fi
+
+if [[ "$1" != "" ]]; then
+  triplet="$1"
+fi
+
+vcpkg_triplet="--triplet ${triplet}-clap-plugins --host-triplet {triplet}-clap-plugins"
+cmake_triplet="-DVCPKG_TARGET_TRIPLET={triplet}-clap-plugins -DCMAKE_VCPKG_HOST_TRIPLET={triplet}-clap-plugins"
 
 vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_triplet install --recurse \
   "qtbase[core,png,doubleconversion,gui,concurrent,appstore-compliant,network,freetype,testlib${QT_FEATURES}]"

0452ec4b5e8ac6654f5581ea7f82f3fe37c4cb98
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-02-04 21:38:09 +0100

    move the build tree to the root on windows

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,23 +9,26 @@ if [[ $(uname) = Linux ]] ; then
   cmake_preset="ninja-vcpkg"
   gui_model=threaded
   triplet=$(uname -m)-linux
+  build_tree=
 elif [[ $(uname) = Darwin ]] ; then
   QT_FEATURES=",harfbuzz"
   cmake_preset="ninja-vcpkg"
   gui_model=local
   triplet=$(uname -m)-osx
+  build_tree=
 else
   QT_FEATURES=""
   cmake_preset="vs-vcpkg"
   gui_model=local
   triplet=$(uname -m)-win
+  build_tree="--x-buildtrees-root=c:\bt"
 fi
 
 if [[ "$1" != "" ]]; then
   triplet="$1"
 fi
 
-vcpkg_triplet="--triplet ${triplet}-cp --host-triplet ${triplet}-cp"
+vcpkg_triplet="--triplet ${triplet}-cp --host-triplet ${triplet}-cp $build_tree"
 cmake_triplet="-DVCPKG_TARGET_TRIPLET=${triplet}-cp -DCMAKE_VCPKG_HOST_TRIPLET=${triplet}-cp"
 
 vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_triplet install --recurse \

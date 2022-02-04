5e7bb3ba7c48a0b397db64b7f0c0e00125ff480d
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-02-04 23:07:57 +0100

    better file names now that we move the directory

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,39 +9,39 @@ if [[ $(uname) = Linux ]] ; then
   cmake_preset="ninja-vcpkg"
   gui_model=threaded
   triplet=$(uname -m)-linux
-  build_tree=
+  buildtrees=vcpkg/buildtrees
 elif [[ $(uname) = Darwin ]] ; then
   QT_FEATURES=",harfbuzz"
   cmake_preset="ninja-vcpkg"
   gui_model=local
   triplet=$(uname -m)-osx
-  build_tree=
+  buildtrees=vcpkg/buildtrees
 else
   QT_FEATURES=""
   cmake_preset="vs-vcpkg"
   gui_model=local
-  triplet=$(uname -m)-win
-  build_tree="--x-buildtrees-root=c:\bt"
+  triplet=$(uname -m)-windows
+  buildtrees="c:\bt"
 fi
 
 if [[ "$1" != "" ]]; then
   triplet="$1"
 fi
 
-vcpkg_triplet="--triplet ${triplet}-cp --host-triplet ${triplet}-cp $build_tree"
-cmake_triplet="-DVCPKG_TARGET_TRIPLET=${triplet}-cp -DCMAKE_VCPKG_HOST_TRIPLET=${triplet}-cp"
+vcpkg_options="--triplet ${triplet}-clap-plugins --host-triplet ${triplet}-clap-plugins --x-buildtrees-root=$buildtrees"
+cmake_options="-DVCPKG_TARGET_TRIPLET=${triplet}-clap-plugins -DCMAKE_VCPKG_HOST_TRIPLET=${triplet}-clap-plugins"
 
-vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_triplet install --recurse \
+vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_options install --recurse \
   "qtbase[core,png,doubleconversion,gui,concurrent,appstore-compliant,network,freetype,testlib,freetype${QT_FEATURES}]"
 
 # save space
-rm -rf vcpkg/buildtrees
+rm -rf $buildtrees
 
-vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_triplet install --recurse \
+vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_options install --recurse \
   qtdeclarative
 
 # save space
-rm -rf vcpkg/buildtrees
+rm -rf $buildtrees
 
-cmake --preset $cmake_preset $cmake_triplet -DCLAP_PLUGIN_GUI_MODEL=$gui_model
+cmake --preset $cmake_preset $cmake_options -DCLAP_PLUGIN_GUI_MODEL=$gui_model
 cmake --build --preset $cmake_preset --config Release --target clap-plugins

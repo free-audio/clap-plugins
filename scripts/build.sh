ee979d26bbffdfb9edb7eedfd076a9a88deb88d5
Alexandre BIQUE <bique.alexandre@gmail.com>
2022-02-08 23:55:00 +0100

    Try to get windows to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,32 +4,40 @@ if [[ ! -x vcpkg/vcpkg ]] ; then
   vcpkg/bootstrap-vcpkg.sh
 fi
 
+cpu="$(uname -m)"
+case "$cpu" in
+x86_64)
+  cpu="x64";;
+i686)
+  cpu="x86";;
+esac
+
 if [[ $(uname) = Linux ]] ; then
   QT_FEATURES=",xcb,xcb-xlib,xkb,xkbcommon-x11,xlib,xrender,fontconfig,harfbuzz"
   cmake_preset="ninja-vcpkg"
   gui_model=threaded
-  triplet=$(uname -m)-linux
+  triplet=$cpu-linux
   buildtrees=vcpkg/buildtrees
 elif [[ $(uname) = Darwin ]] ; then
   QT_FEATURES=",harfbuzz"
   cmake_preset="ninja-vcpkg"
   gui_model=local
-  triplet=$(uname -m)-osx
+  triplet=$cpu-osx
   buildtrees=vcpkg/buildtrees
 else
   QT_FEATURES=""
   cmake_preset="vs-vcpkg"
   gui_model=local
-  triplet=$(uname -m)-windows
-  buildtrees="c:\bt"
+  triplet=$cpu-win
+  buildtrees="C:\B"
 fi
 
 if [[ "$1" != "" ]]; then
   triplet="$1"
 fi
 
-vcpkg_options="--triplet ${triplet}-clap-plugins --host-triplet ${triplet}-clap-plugins --x-buildtrees-root=$buildtrees"
-cmake_options="-DVCPKG_TARGET_TRIPLET=${triplet}-clap-plugins -DCMAKE_VCPKG_HOST_TRIPLET=${triplet}-clap-plugins"
+vcpkg_options="--triplet ${triplet}-cp --host-triplet ${triplet}-cp --x-buildtrees-root=$buildtrees"
+cmake_options="-DVCPKG_TARGET_TRIPLET=${triplet}-cp -DCMAKE_VCPKG_HOST_TRIPLET=${triplet}-cp"
 
 vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_options install --recurse \
   "qtbase[core,png,doubleconversion,gui,concurrent,appstore-compliant,network,freetype,testlib,freetype${QT_FEATURES}]"

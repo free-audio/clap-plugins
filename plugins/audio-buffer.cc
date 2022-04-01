b638796ea22de2bec1e7e5f861f0314c33d8d557
Alexandre Bique <bique.alexandre@gmail.com>
2022-04-01 17:41:54 +0200

    Work on audio buffers

--- plugins/audio-buffer.cc
+++ plugins/audio-buffer.cc
@@ -5,9 +5,8 @@ namespace clap {
    void AudioBuffer<T>::fromClap(const clap_audio_buffer *buffer,
                                  uint32_t frameOffset,
                                  uint32_t frameCount) noexcept {
-      const uint32_t allChannelsConstantMask = (1 << _channelCount) - 1;
-      const bool allConstant =
-         (buffer->constant_mask & allChannelsConstantMask) == allChannelsConstantMask;
+      const uint32_t allConstantMask = (1 << _channelCount) - 1;
+      const bool allConstant = (buffer->constant_mask & allConstantMask) == allConstantMask;
 
       if (allConstant) {
          if (buffer->data32) {
@@ -22,13 +21,21 @@ namespace clap {
       }
 
       if (buffer->data32) {
-         for (uint32_t c = 0; c < _channelCount; ++c)
-            for (uint32_t i = 0; i < _frameCount; ++i)
-               _data[i * _channelCount + c] = static_cast<T>(buffer->data32[c][i]);
+         for (uint32_t c = 0; c < _channelCount; ++c) {
+            const uint32_t stride = (buffer->constant_mask & (1 << c)) ? 1 : 0;
+            for (uint32_t i = 0; i < _frameCount; ++i) {
+               const uint32_t index = stride * (i + frameOffset);
+               _data[i * _channelCount + c] = static_cast<T>(buffer->data32[c][index]);
+            }
+         }
       } else {
-         for (uint32_t c = 0; c < _channelCount; ++c)
-            for (uint32_t i = 0; i < _frameCount; ++i)
-               _data[i * _channelCount + c] = static_cast<T>(buffer->data64[c][i]);
+         for (uint32_t c = 0; c < _channelCount; ++c) {
+            const uint32_t stride = (buffer->constant_mask & (1 << c)) ? 1 : 0;
+            for (uint32_t i = 0; i < _frameCount; ++i) {
+               const uint32_t index = stride * (i + frameOffset);
+               _data[i * _channelCount + c] = static_cast<T>(buffer->data64[c][index]);
+            }
+         }
       }
       _stride = _channelCount;
    }
@@ -36,7 +43,64 @@ namespace clap {
    template <typename T>
    void AudioBuffer<T>::toClap(clap_audio_buffer *buffer,
                                uint32_t frameOffset,
-                               uint32_t frameCount) noexcept {}
+                               uint32_t frameCount) noexcept {
+      const uint32_t allConstantMask = (1 << _channelCount) - 1;
+
+      // This is the first block and it is constant
+      if (_stride == 0 && frameOffset == 0) {
+         if (buffer->data32) {
+            for (uint32_t c = 0; c < _channelCount; ++c)
+               buffer->data32[c][0] = static_cast<float>(_data[c]);
+         } else {
+            for (uint32_t c = 0; c < _channelCount; ++c)
+               buffer->data64[c][0] = static_cast<double>(_data[c]);
+         }
+         buffer->constant_mask = allConstantMask;
+         return;
+      }
+
+      // This isn't the first block and it is constant, and the block before were constant too
+      const bool allConstant = (buffer->constant_mask & allConstantMask) == allConstantMask;
+      if (_stride == 0 && frameOffset != 0 && allConstant) {
+         // check if the constant value is the same as in the previous block
+         bool same = true;
+         if (buffer->data32) {
+            for (uint32_t c = 0; same && c < _channelCount; ++c)
+               same = (_data[c] == buffer->data32[c][0]);
+         } else {
+            for (uint32_t c = 0; c < _channelCount; ++c)
+               same = (_data[c] == buffer->data32[c][0]);
+         }
+
+         if (same)
+            return;
+      }
+
+      // The previous block were constant but this one isn't, we need to fill the buffer
+      if (allConstant && _stride > 0) {
+         if (buffer->data32) {
+            for (uint32_t c = 0; c < _channelCount; ++c)
+               for (uint32_t i = 0; i < frameOffset; ++i)
+                  buffer->data32[c][i] = buffer->data32[c][0];
+         } else {
+            for (uint32_t c = 0; c < _channelCount; ++c)
+               for (uint32_t i = 0; i < frameOffset; ++i)
+                  buffer->data64[c][i] = buffer->data64[c][0];
+         }
+      }
+
+      // Copy this block
+      if (buffer->data32) {
+         for (uint32_t c = 0; c < _channelCount; ++c)
+            for (uint32_t i = 0; i < _frameCount; ++i)
+               buffer->data32[c][i + frameOffset] = static_cast<float>(_data[i * _stride + c]);
+      } else {
+         for (uint32_t c = 0; c < _channelCount; ++c)
+            for (uint32_t i = 0; i < _frameCount; ++i)
+               buffer->data64[c][i + frameOffset] = static_cast<double>(_data[i * _stride + c]);
+      }
+      buffer->constant_mask = 0;
+   }
 
    template class AudioBuffer<float>;
    template class AudioBuffer<double>;

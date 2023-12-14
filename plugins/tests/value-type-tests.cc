a7f24f0b3079eb93e312caa262276273ccd4ace6
Alexandre Bique <bique.alexandre@gmail.com>
2023-12-14 15:53:11 +0100

    Get the unit test to work

--- plugins/tests/value-type-tests.cc
+++ plugins/tests/value-type-tests.cc
@@ -1,13 +1,14 @@
 #include "../value-types/decibel-value-type.hh"
 
 #include <catch2/catch_all.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 namespace {
 
    CATCH_TEST_CASE("value-type - decibel") {
       clap::DecibelValueType vt;
 
-      CATCH_CHECK(vt.toParam(vt.toEngine(3)) == 3);
+      CATCH_CHECK_THAT(vt.toParam(vt.toEngine(3)), Catch::Matchers::WithinAbs(3, 1e-6));
    }
 
 } // namespace

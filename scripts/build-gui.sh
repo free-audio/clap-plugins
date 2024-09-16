9a29a326c93edfd85727d418cd6c763592907239
Alexandre Bique <bique.alexandre@gmail.com>
2024-09-16 16:00:16 +0200

    Update build script

--- scripts/build-gui.sh
+++ scripts/build-gui.sh
@@ -1,4 +1,4 @@
-#! /bin/bash -e
+#! /bin/bash -xe
 
 cpu="$(uname -m)"
 case "$cpu" in
@@ -9,7 +9,7 @@ i686)
 esac
 
 if [[ $(uname) = Linux ]] ; then
-  QT_FEATURES=",xcb,xcb-xlib,xkb,xkbcommon-x11,xlib,xrender,fontconfig,harfbuzz"
+  QT_FEATURES=",xcb,xcb-xlib,xkb,xkbcommon-x11,xlib,xrender,fontconfig,harfbuzz,vulkan"
   cmake_preset="ninja-vcpkg"
   gui_model=threaded
   triplet=$cpu-linux
@@ -32,25 +32,17 @@ if [[ "$1" != "" ]]; then
   triplet="$1"
 fi
 
-vcpkg_options="--triplet ${triplet}-cp --host-triplet ${triplet}-cp --x-buildtrees-root=$buildtrees"
+vcpkg_options="--triplet ${triplet}-cp --host-triplet ${triplet}-cp --x-buildtrees-root=$buildtrees --allow-unsupported"
 cmake_options="-DVCPKG_TARGET_TRIPLET=${triplet}-cp -DVCPKG_HOST_TRIPLET=${triplet}-cp"
 
 if [[ ! -x vcpkg/vcpkg ]] ; then
   vcpkg/bootstrap-vcpkg.sh
 else
-  vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_option upgrade --no-dry-run --debug
+  vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets upgrade $vcpkg_options --no-dry-run --debug
 fi
 
-vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_options install --recurse --debug \
-  "qtbase[core,png,doubleconversion,gui,concurrent,appstore-compliant,network,freetype,testlib,freetype${QT_FEATURES}]"
-
-# save space
-rm -rf $buildtrees
-
-vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_options install --recurse --debug \
-  qtdeclarative
-
-vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets $vcpkg_options upgrade --debug
+vcpkg/vcpkg --overlay-triplets=vcpkg-overlay/triplets install $vcpkg_options --recurse --clean-after-build --debug \
+  "qtbase[core,png,brotli,zstd,doubleconversion,gui,concurrent,appstore-compliant,network,freetype,testlib,freetype${QT_FEATURES}]" qtdeclarative
 
 # save space
 rm -rf $buildtrees

89530c648510532909f813a9602d7a015d7e7ca8
Alexandre Bique <bique.alexandre@gmail.com>
2024-09-24 00:54:06 +0200

    update windows build script

--- scripts/build-gui.ps1
+++ scripts/build-gui.ps1
@@ -1,4 +1,4 @@
 vcpkg/bootstrap-vcpkg.bat
 vcpkg/vcpkg.exe --overlay-triplets=vcpkg-overlay/triplets install --triplet x64-win-cp --host-triplet x64-win-cp '--x-buildtrees-root=C:\B' --allow-unsupported --recurse --clean-after-build 'qtbase[core,png,brotli,zstd,doubleconversion,gui,concurrent,appstore-compliant,network,freetype,testlib,freetype]' qtdeclarative
-cmake --preset $cmake_preset $cmake_options -DCLAP_PLUGIN_GUI_MODEL=$gui_model
-cmake --build --preset $cmake_preset --config Release --target clap-plugins
+cmake --preset vs-vcpkg -DVCPKG_TARGET_TRIPLET=x64-win-cp -DVCPKG_HOST_TRIPLET=x64-win-cp -DCLAP_PLUGIN_GUI_MODEL=local
+cmake --build --preset vs-vcpkg --config Release --target clap-plugins

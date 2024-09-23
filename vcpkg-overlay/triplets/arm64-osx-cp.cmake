e7e9f00bf44926982ed7a6687f4e971c8790c3e2
Alexandre Bique <bique.alexandre@gmail.com>
2024-09-24 00:44:43 +0200

    set the osx deployment target

--- vcpkg-overlay/triplets/arm64-osx-cp.cmake
+++ vcpkg-overlay/triplets/arm64-osx-cp.cmake
@@ -10,6 +10,8 @@ set(VCPKG_OSX_ARCHITECTURES arm64)
 set(VCPKG_C_FLAGS_RELEASE "-Os -fomit-frame-pointer")
 set(VCPKG_CXX_FLAGS_RELEASE "-Os -fomit-frame-pointer")
 
+set(VCPKG_OSX_DEPLOYMENT_TARGET 12.0)
+
 #if(NOT PORT MATCHES "harfbuzz")
     # harfbuzz is badly packaged for now, so we also make the debug build
     # https://github.com/microsoft/vcpkg/issues/22680

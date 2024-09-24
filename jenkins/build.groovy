72b6321f52715ac5f4be5724a1070b986465cb83
Alexandre Bique <bique.alexandre@gmail.com>
2024-09-24 20:12:55 +0200

    make a zip

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -68,7 +68,8 @@ pipeline {
           }
           post {
             always {
-              archiveArtifacts artifacts: 'builds/ninja-vcpkg/plugins/Release/clap-plugins.clap', fingerprint: true
+              zip zipFile: 'linux-clap-plugins.zip', archive: true: overwrite: true,
+                glob: 'builds/ninja-vcpkg/plugins/Release/clap-plugins.clap'
               zip zipFile: 'linux-vcpkg-logs.zip', archive: true, overwrite: true,
                 glob: 'vcpkg/buildtrees/**/*.log, vcpkg/installed/vcpkg/issue_body.md'
             }

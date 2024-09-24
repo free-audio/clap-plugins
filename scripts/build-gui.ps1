c896d443791d782c2e1c5d6200e6b43b5f272167
Alexandre Bique <bique.alexandre@gmail.com>
2024-09-24 22:52:29 +0200

    Try without the short buildtree path

--- scripts/build-gui.ps1
+++ scripts/build-gui.ps1
@@ -1,4 +1,6 @@
 vcpkg/bootstrap-vcpkg.bat
-vcpkg/vcpkg.exe --overlay-triplets=vcpkg-overlay/triplets install --triplet x64-win-cp --host-triplet x64-win-cp '--x-buildtrees-root=C:\B' --allow-unsupported --recurse --clean-after-build 'qtbase[core,png,brotli,zstd,doubleconversion,gui,concurrent,appstore-compliant,network,freetype,testlib,freetype]' qtdeclarative
+
+# '--x-buildtrees-root=C:\B'
+vcpkg/vcpkg.exe --overlay-triplets=vcpkg-overlay/triplets install --triplet x64-win-cp --host-triplet x64-win-cp --allow-unsupported --recurse --clean-after-build 'qtbase[core,png,brotli,zstd,doubleconversion,gui,concurrent,appstore-compliant,network,freetype,testlib,freetype]' qtdeclarative
 cmake --preset vs-vcpkg -DVCPKG_TARGET_TRIPLET=x64-win-cp -DVCPKG_HOST_TRIPLET=x64-win-cp -DCLAP_PLUGIN_GUI_MODEL=local
 cmake --build --preset vs-vcpkg --config Release --target clap-plugins

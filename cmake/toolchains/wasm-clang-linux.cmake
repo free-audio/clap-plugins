832d22e63e74783d7fee6c97d8a2fdaa5af8f4ca
Alexandre Bique <bique.alexandre@gmail.com>
2024-11-22 17:30:26 +0100

    Start wasm port

---
+++ cmake/toolchains/wasm-clang-linux.cmake
@@ -0,0 +1,22 @@
+set(CMAKE_SYSTEM_NAME WASI)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR wasm32)
+
+set(triple "wasm32-unknown-wasi")
+
+set(CMAKE_C_COMPILER "/usr/bin/clang")
+set(CMAKE_CXX_COMPILER "/usr/bin/clang++")
+set(CMAKE_ASM_COMPILER "/usr/bin/clang")
+set(CMAKE_AR "/usr/bin/llvm-ar")
+set(CMAKE_RANLIB "/usr/bin/llvm-ranlib")
+
+set(CMAKE_C_COMPILER_TARGET ${triple})
+set(CMAKE_CXX_COMPILER_TARGET ${triple})
+set(CMAKE_ASM_COMPILER_TARGET ${triple})
+
+# Don't look in the sysroot for executables to run during the build
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# Only look in the sysroot (not in the host paths) for the rest
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
